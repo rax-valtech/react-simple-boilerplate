ce5e9aa1217c99bc36fe388b4c0b9ede83b79f0c
Rikard Axelsson <rikard.axelsson@valtech.se>
2016-10-12 08:15:50 +0200

    Initial

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require("path");
+
+module.exports = {
+  devtool: "eval",
+  entry: [
+    "webpack-dev-server/client?http://localhost:3000",
+    "./src/index.jsx"
+  ],
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/build/"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ["babel"],
+        include: path.join(__dirname, "src")
+      },
+      {
+        test: /\.scss$/,
+        loaders: ["style", "css", "sass"]
+      }
+    ]
+  }
+}
