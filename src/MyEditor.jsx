cad6f1eb0034a1539e076cbf1cc6ae1190599dbe
Rikard Axelsson <rikard.axelsson@valtech.se>
2016-11-11 14:09:18 +0100

    Add rawcontent and custom block render

--- src/MyEditor.jsx
+++ src/MyEditor.jsx
@@ -1,14 +1,68 @@
-import React from "react";
-import {Editor, EditorState} from "draft-js";
+import React, { PropTypes } from "react";
+import Draft, { Editor, EditorState, convertFromRaw } from "draft-js";
+import Immutable from "immutable";
+
+class MyCustomBlock extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="my-custom-block">
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+MyCustomBlock.propTypes = {
+  children: PropTypes.any
+}
+
+const blockRenderMap = Immutable.Map({
+  "my-custom-block": {
+    element: "section",
+    wrapper: <MyCustomBlock />
+  }
+});
+
+const extendedBlockRenderMap = Draft.DefaultDraftBlockRenderMap.merge(blockRenderMap);
+
+const rawContent = {
+  blocks: [
+    {
+      text: ("This block is unstyled"),
+      type: "unstyled"
+    }, {
+      text: (
+        "This is an 'immutable' entity: Superman. Deleting any " +
+        "characters will delete the entire entity. Adding characters " +
+        "will remove the entity from the range."
+      ),
+      type: "my-custom-block"
+    }, {
+      text: ("This block is unstyled"),
+      type: "unstyled"
+    }],
+  entityMap: {
+
+  }
+};
+
+const blocks = convertFromRaw(rawContent);
 
 export default class MyEditor extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {editorState: EditorState.createEmpty()};
-    this.onChange = (editorState) => this.setState({editorState});
+    this.state = {
+      editorState: EditorState.createWithContent(
+        blocks
+      )
+    };
+    this.onChange = (editorState) => this.setState({ editorState });
   }
   render() {
     const {editorState} = this.state;
-    return <Editor editorState={editorState} onChange={this.onChange} />;
+    return <Editor editorState={editorState} onChange={this.onChange} blockRenderMap={extendedBlockRenderMap} />;
   }
 }
